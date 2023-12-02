493491bbe9f1369200c9635904719ac9ab2a7d6e
shoaib <shoaibhossain24@gmail.com>
2023-12-03 05:27:52 +0600

    Add image to Navbar and update page content

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import Link from "next/link"
-
+import Image from "next/image";
+import Link from "next/link";
+import hossain from "./hossain.png";
 
 const Navbar = () => {
   return (
     <nav>
-    <h1>Help Desk</h1>
-    <Link href={"/"}>Dashboard</Link>
-    <Link href={"/tickets"}>Tickets</Link>
-  </nav>
-  )
-}
+      <Image
+        src={hossain}
+        alt="Hossain-Pic"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Help Desk</h1>
+      
+      <Link href={"/"}>Dashboard</Link>
+      <Link href={"/tickets"}>Tickets</Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
