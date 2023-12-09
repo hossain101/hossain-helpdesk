9d0baf44800d0d2a19d9cd1cd7e1259119608a75
shoaib <shoaibhossain24@gmail.com>
2023-12-10 02:39:53 +0600

    Update API endpoint in TicketList.jsx

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import { notFound } from "next/navigation";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/tickets"||"https://hossaindesk.onrender.com", {
+  const res = await fetch("http://localhost:3000/api/tickets"||"https://hossaindesk.onrender.com/api/tickets", {
     cache: "no-store",
   });
 
